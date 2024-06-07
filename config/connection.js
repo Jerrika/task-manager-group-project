85c7e9e264addaec976731ba29407d30d6ad572a
Brooke <brookedunlap97@gmail.com>
2024-06-06 21:24:07 -0500

    Set up Sequelize to connect to a PostgreSQL database. Created db. Added starting code to define the User and Task db tables

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+//Sets up and exports a Sequelize instance for connecting to a PostgreSQL database
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
+
+module.exports = sequelize;
